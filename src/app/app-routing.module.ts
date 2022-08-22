5b23e5e0bacd818fc39be0001495122163661e38
gokhnesen <gokhan_esen@hotmail.com.tr>
2022-08-22 12:29:53 +0300

    Routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home/home.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { ListsComponent } from './lists/lists/lists.component';
+import { MessagesComponent } from './messages/messages/messages.component';
+
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'members', component:MemberListComponent},
+  {path:'members/:id', component:MemberDetailComponent},
+  {path:'lists', component:ListsComponent},
+  {path:'messages', component:MessagesComponent},
+  {path:'**', component:HomeComponent,pathMatch:'full'},
+
+
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/nav/nav/nav.component.ts
@@ -0,0 +1,37 @@
+import { User } from './../../_models/user';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AccountService } from 'src/app/_services/account.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  model:any={}
+  constructor(
+    public accountService:AccountService,
+    private router:Router
+  ) { }
+
+  ngOnInit(): void {
+
+  }
+login(){
+  this.accountService.login(this.model).subscribe(response=>{
+    this.router.navigateByUrl('/members');
+  },error=>{
+    console.log(error);
+  })
+}
+
+logout(){
+  this.accountService.logout();
+  this.router.navigateByUrl('/')
+}
+
+
+}
