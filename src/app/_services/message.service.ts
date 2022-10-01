aca0de17b3ca2130fd8b34bc9a8339f65635acad
gokhnesen <gokhan_esen@hotmail.com.tr>
2022-10-01 11:14:37 +0300

    Delete Messages

--- src/app/_services/message.service.ts
+++ src/app/_services/message.service.ts
@@ -53,4 +53,8 @@ return params;
 sendMessage(username:string,content:string){
   return this.http.post<Message>(this.baseUrl+'messages',{recipientUsername:username,content})
 }
+
+deleteMessage(id:number){
+  return this.http.delete(this.baseUrl + 'messages/' + id);
+}
 }
